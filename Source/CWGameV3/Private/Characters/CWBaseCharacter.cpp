048655d27689e63606748e94da526a4785146546
b97wright <110114784+b97wright@users.noreply.github.com>
2025-03-26 18:32:44 -0400

    8. Basic Class Structure Part 2 Finished
    https://docs.google.com/document/d/1HJy9v9DZdRySyXtq6AzRS7Meg5sf4b8BXzMiXI637jk/edit?tab=t.0#heading=h.qyv1ik22685x

--- Source/CWGameV3/Private/Characters/CWBaseCharacter.cpp
+++ Source/CWGameV3/Private/Characters/CWBaseCharacter.cpp
@@ -7,7 +7,10 @@
 ACWBaseCharacter::ACWBaseCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bStartWithTickEnabled = false;
+
+	GetMesh()->bReceivesDecals = false;
 
 }
 
---
+++ Source/CWGameV3/Public/Characters/ConeHeroCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/CWBaseCharacter.h"
+#include "ConeHeroCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CWGAMEV3_API AConeHeroCharacter : public ACWBaseCharacter
+{
+	GENERATED_BODY()
+	
+};
